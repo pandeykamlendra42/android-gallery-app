ce3018b57d3819410d0b8cb64152ec001f2b9970
Kamlendra Pandey <kamlendra@niro.money>
2022-11-23 19:47:19 +0530

    Details Page UI Improvements

--- app/src/main/java/com/nasa/gallery/data/model/Image.kt
+++ app/src/main/java/com/nasa/gallery/data/model/Image.kt
@@ -3,7 +3,8 @@ package com.nasa.gallery.data.model
 data class Image(
     val title: String,
     val explanation: String,
-    val hdUrl: String,
+    val hdurl: String,
     val url: String,
     val date: String,
+    val copyright: String,
 )
